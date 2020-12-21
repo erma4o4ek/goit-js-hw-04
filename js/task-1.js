792489b09c976b35bac29262b62a213300d05384
erma4o4ek <57236378+erma4o4ek@users.noreply.github.com>
2020-12-22 01:12:42 +0300

    init

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
+};
+
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
